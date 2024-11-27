cc3e270a19eb838c317c8cd7eb324d997312c059
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-26 22:11:35 -0600

    Update ___inti__.py

--- Movies_Graph.\\\/mod-SCHE/___inti__.py
+++ Movies_Graph.\\\/mod-SCHE/___inti__.py
@@ -5,7 +5,6 @@ from flask_migrate import Migrate
 from graphql_server.flask import GraphQLView
 from app.schema import schema
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
